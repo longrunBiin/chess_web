6e9671cfbbcd6e8b15d2b018110c49bf4cdc9685
jangsubin <eltm521@naver.com>
2025-03-13 11:54:28 +0900

    CSS 파일 경로 수정 및 Vite 설정 업데이트

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 5500
-  }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  publicDir: 'public'
 }) 
